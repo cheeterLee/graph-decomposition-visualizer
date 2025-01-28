a504213c10351e1d63a7fb0755d9697e83027ff9
cheeterLeee <johnson991210@gmail.com>
2025-01-28 20:40:55 +0000

    downgrade existing code from react--router@v7 to remix, downgrade from react 19 to 18

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,138 +1,483 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
 
+import React from "react";
+import * as d3 from "d3";
+import { useSet } from "~/hooks/useSet";
+import { useMap } from "~/hooks/useMap";
+
+import {
+	Grab,
+	MousePointer2,
+	RotateCcw,
+	Plus,
+	Palette,
+	Trash2,
+	Spline,
+} from "lucide-react";
+
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import "highlight.js/styles/base16/classic-light.css";
+import { Button } from "~/components/ui/button";
+import { Separator } from "~/components/ui/separator";
+
+import { useLoaderData } from "@remix-run/react";
+
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [
+		{ title: "Graph Decomposition Visualizer" },
+		{
+			name: "description",
+			content: "Visualization tool for graph decomposition algorithms",
+		},
+	];
 };
 
-export default function Index() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to <span className="sr-only">Remix</span>
-          </h1>
-          <div className="h-[144px] w-[434px]">
-            <img
-              src="/logo-light.png"
-              alt="Remix"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src="/logo-dark.png"
-              alt="Remix"
-              className="hidden w-full dark:block"
-            />
-          </div>
-        </header>
-        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-          <p className="leading-6 text-gray-700 dark:text-gray-200">
-            What&apos;s next?
-          </p>
-          <ul>
-            {resources.map(({ href, text, icon }) => (
-              <li key={href}>
-                <a
-                  className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {icon}
-                  {text}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
+hljs.registerLanguage("javascript", javascript);
+
+function CodeSnippet() {
+	const codeRef = React.useRef<HTMLElement | null>(null);
+
+	React.useEffect(() => {
+		if (!codeRef.current) return;
+		hljs.highlightBlock(codeRef.current);
+	}, []);
+
+	return (
+		<pre>
+			<code
+				className="javascript bg-transparent p-2 text-sm"
+				style={{ background: "transparent" }}
+				ref={codeRef}
+			>
+				{`
+function graphDecomposition(adjList) {
+    const visited = new Set();
+    const components = [];
+
+    function dfs(node, currentComponent) {
+        visited.add(node);
+        currentComponent.add(node);
+
+        const neighbors = adjList[node] || [];
+        for (const neighbor of neighbors) {
+            if (!visited.has(neighbor)) {
+                dfs(neighbor, currentComponent);
+            }
+        }
+    }
+
+    for (const node in adjList) {
+        if (!visited.has(node)) {
+            const currentComponent = new Set();
+            dfs(node, currentComponent);
+            components.push(currentComponent);
+        }
+    }
+
+    return components;
+}`}
+			</code>
+		</pre>
+	);
 }
 
-const resources = [
-  {
-    href: "https://remix.run/start/quickstart",
-    text: "Quick Start (5 min)",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M8.51851 12.0741L7.92592 18L15.6296 9.7037L11.4815 7.33333L12.0741 2L4.37036 10.2963L8.51851 12.0741Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://remix.run/start/tutorial",
-    text: "Tutorial (30 min)",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M4.561 12.749L3.15503 14.1549M3.00811 8.99944H1.01978M3.15503 3.84489L4.561 5.2508M8.3107 1.70923L8.3107 3.69749M13.4655 3.84489L12.0595 5.2508M18.1868 17.0974L16.635 18.6491C16.4636 18.8205 16.1858 18.8205 16.0144 18.6491L13.568 16.2028C13.383 16.0178 13.0784 16.0347 12.915 16.239L11.2697 18.2956C11.047 18.5739 10.6029 18.4847 10.505 18.142L7.85215 8.85711C7.75756 8.52603 8.06365 8.21994 8.39472 8.31453L17.6796 10.9673C18.0223 11.0653 18.1115 11.5094 17.8332 11.7321L15.7766 13.3773C15.5723 13.5408 15.5554 13.8454 15.7404 14.0304L18.1868 16.4767C18.3582 16.6481 18.3582 16.926 18.1868 17.0974Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://remix.run/docs",
-    text: "Remix Docs",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 24 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M15.0686 1.25995L14.5477 1.17423L14.2913 1.63578C14.1754 1.84439 14.0545 2.08275 13.9422 2.31963C12.6461 2.16488 11.3406 2.16505 10.0445 2.32014C9.92822 2.08178 9.80478 1.84975 9.67412 1.62413L9.41449 1.17584L8.90333 1.25995C7.33547 1.51794 5.80717 1.99419 4.37748 2.66939L4.19 2.75793L4.07461 2.93019C1.23864 7.16437 0.46302 11.3053 0.838165 15.3924L0.868838 15.7266L1.13844 15.9264C2.81818 17.1714 4.68053 18.1233 6.68582 18.719L7.18892 18.8684L7.50166 18.4469C7.96179 17.8268 8.36504 17.1824 8.709 16.4944L8.71099 16.4904C10.8645 17.0471 13.128 17.0485 15.2821 16.4947C15.6261 17.1826 16.0293 17.8269 16.4892 18.4469L16.805 18.8725L17.3116 18.717C19.3056 18.105 21.1876 17.1751 22.8559 15.9238L23.1224 15.724L23.1528 15.3923C23.5873 10.6524 22.3579 6.53306 19.8947 2.90714L19.7759 2.73227L19.5833 2.64518C18.1437 1.99439 16.6386 1.51826 15.0686 1.25995ZM16.6074 10.7755L16.6074 10.7756C16.5934 11.6409 16.0212 12.1444 15.4783 12.1444C14.9297 12.1444 14.3493 11.6173 14.3493 10.7877C14.3493 9.94885 14.9378 9.41192 15.4783 9.41192C16.0471 9.41192 16.6209 9.93851 16.6074 10.7755ZM8.49373 12.1444C7.94513 12.1444 7.36471 11.6173 7.36471 10.7877C7.36471 9.94885 7.95323 9.41192 8.49373 9.41192C9.06038 9.41192 9.63892 9.93712 9.6417 10.7815C9.62517 11.6239 9.05462 12.1444 8.49373 12.1444Z"
-          strokeWidth="1.5"
-        />
-      </svg>
-    ),
-  },
-];
+type Vertex = {
+	id: number;
+	cx: number;
+	cy: number;
+	neighbors: number[];
+};
+
+const offSet = 100;
+
+export function loader() {
+	const graph = new Map<number, Vertex>();
+	let maxId = 0;
+
+	const data: Vertex[] = [
+		{
+			id: 1,
+			cx: 200,
+			cy: 200,
+			neighbors: [2],
+		},
+		{
+			id: 2,
+			cx: 300,
+			cy: 300,
+
+			neighbors: [1],
+		},
+		{
+			id: 3,
+			cx: 200,
+			cy: 300,
+			neighbors: [1],
+		},
+	];
+
+	const edges: [number, number][] = [
+		...data.reduce((prev, curr) => {
+			graph.set(curr.id, curr);
+			maxId = Math.max(curr.id, maxId);
+			for (const nei of curr.neighbors) {
+				if (
+					!prev.has(`${curr.id}-${nei}`) &&
+					!prev.has(`${nei}-${curr.id}`)
+				) {
+					prev.add(`${curr.id}-${nei}`);
+				}
+			}
+			return prev;
+		}, new Set<string>()),
+	].map((e) => {
+		let arr = e.split("-");
+		return [+arr[0], +arr[1]] as [number, number];
+	});
+
+	return { data, e: edges, g: graph, maxId };
+}
+
+export default function Index() {
+	const { data, e, g, maxId } = useLoaderData<typeof loader>();
+
+	const [vertices, setVertices] = React.useState<Vertex[]>(data);
+	const [edges, setEdges] = React.useState<[number, number][]>(e);
+
+	const graph = useMap<number, Vertex>(Array.from(g.entries()));
+
+	const edgesSet = useSet<string>(edges.map((e) => `${e[0]}-${e[1]}`));
+
+	const [nextVertexId, setNextVertexId] = React.useState<number>(maxId + 1);
+
+	const [highlightedElement, setHighlightedElement] = React.useState<{
+		type: "node" | "edge";
+		id: number | string; // For nodes, use the node ID; for edges, use a string like '1-2'
+	} | null>(null);
+
+	const isNodeSelected = highlightedElement?.type === "node";
+
+	const [isAddEdgeMode, setIsAddEdgeMode] = React.useState<boolean>(false);
+
+	const handleResetGraph = () => {
+		setVertices(data);
+		setEdges(e);
+	};
+
+	const handleAddVertex = () => {
+		if (!svgContainerRef.current) return;
+		const newVertex = {
+			id: nextVertexId,
+			cx:
+				svgContainerRef.current.clientWidth / 2 +
+				Math.pow(-1, Math.floor(Math.random() * 5)) *
+					Math.random() *
+					offSet,
+			cy:
+				svgContainerRef.current.clientHeight / 2 +
+				Math.pow(-1, Math.floor(Math.random() * 5)) *
+					Math.random() *
+					offSet,
+
+			neighbors: [],
+		};
+		setVertices([...vertices, newVertex]);
+		setNextVertexId((prevId) => prevId + 1);
+	};
+
+	const handleAddEdge = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.stopPropagation();
+		if (!highlightedElement || highlightedElement.type !== "node") return;
+		setIsAddEdgeMode(true);
+	};
+
+	const dragStarted = (event: d3.D3DragEvent<Element, Vertex, unknown>) => {
+		const group = d3
+			.select(event.sourceEvent.target.parentNode as SVGGElement)
+			.raise();
+
+		group.select("circle").attr("stroke", "#FF7F50");
+		group.select("text").attr("stroke", "#FF7F50");
+	};
+
+	const dragged = (event: d3.D3DragEvent<Element, Vertex, unknown>) => {
+		const nodeDragged = event.subject as Vertex;
+		setVertices((prevVertices) =>
+			prevVertices.map((prevVertex) =>
+				prevVertex.id !== nodeDragged.id
+					? prevVertex
+					: { ...prevVertex, cx: event.x, cy: event.y }
+			)
+		);
+	};
+
+	const dragEnded = (event: d3.D3DragEvent<Element, Vertex, unknown>) => {
+		const group = d3
+			.select(event.sourceEvent.target.parentNode as SVGGElement)
+			.raise();
+
+		group.select("circle").attr("stroke", "#5f9ea0");
+		group.select("text").attr("stroke", "#5f9ea0");
+	};
+
+	const svgContainerRef = React.useRef<HTMLDivElement | null>(null);
+
+	const svgRef = React.useRef<d3.Selection<
+		SVGSVGElement,
+		undefined,
+		null,
+		undefined
+	> | null>(null);
+
+	const linksGroupRef = React.useRef<SVGGElement | null>(null);
+	const nodesGroupRef = React.useRef<SVGGElement | null>(null);
+
+	// Effect runs on initial mount to draw SVG
+	React.useEffect(() => {
+		if (!svgContainerRef.current) return;
+
+		const svg = d3
+			.create("svg")
+			.attr("width", svgContainerRef.current.clientWidth)
+			.attr("height", svgContainerRef.current.clientHeight);
+		svgRef.current = svg;
+
+		const linksGroup = svg
+			.append("g")
+			.attr("class", "link")
+			.attr("stroke", "#DEB887")
+			.attr("stroke-width", 5);
+
+		linksGroupRef.current = linksGroup.node();
+
+		const nodesGroup = svg
+			.append("g")
+			.attr("class", "node")
+			.attr("fill", "#fff")
+			.attr("stroke", "#5f9ea0")
+			.attr("stroke-width", 3);
+
+		nodesGroupRef.current = nodesGroup.node();
+
+		const svgNode = svg.node();
+		if (!svgNode) return;
+		svgContainerRef.current.appendChild(svgNode);
+	}, []);
+
+	// Effect to re-draw SVG upon user interaction
+	React.useEffect(() => {
+		if (!nodesGroupRef.current || !linksGroupRef.current) return;
+
+		const linksGroup = d3.select(linksGroupRef.current);
+		const nodesGroup = d3.select(nodesGroupRef.current);
+
+		// Data join for links (edges)
+		linksGroup
+			.selectAll<SVGLineElement, [number, number]>("line")
+			.data(edges, (d: [number, number]) => `${d[0]}-${d[1]}`) // Key function for unique identification
+			.join("line")
+			.attr("x1", (e) => vertices.find((v) => v.id === e[0])?.cx ?? 0)
+			.attr("y1", (e) => vertices.find((v) => v.id === e[0])?.cy ?? 0)
+			.attr("x2", (e) => vertices.find((v) => v.id === e[1])?.cx ?? 0)
+			.attr("y2", (e) => vertices.find((v) => v.id === e[1])?.cy ?? 0)
+			.attr("stroke", "#DEB887")
+			.attr("stroke-width", 3)
+			.on("click", function (event: MouseEvent, d) {
+				event.stopPropagation();
+				const edgeKey = `${d[0]}-${d[1]}`;
+				if (
+					highlightedElement?.type === "edge" &&
+					highlightedElement.id === edgeKey
+				) {
+					setHighlightedElement(null);
+				} else {
+					setHighlightedElement({ type: "edge", id: edgeKey });
+				}
+			})
+			.classed(
+				"highlighted-edge",
+				(d) =>
+					highlightedElement?.type === "edge" &&
+					highlightedElement.id === `${d[0]}-${d[1]}`
+			);
+
+		// Data join for nodes (vertices)
+		nodesGroup
+			.selectAll<SVGGElement, Vertex>("g.node-group")
+			.data(vertices, (d: Vertex) => d.id)
+			.join(
+				(enter) => {
+					const group = enter.append("g").attr("class", "node-group");
+
+					group
+						.append("circle")
+						.attr("r", 15)
+						.attr("fill", "#fff")
+						.attr("stroke", "#5f9ea0")
+						.attr("stroke-width", 3);
+
+					group
+						.append("text")
+						.text((d) => d.id.toString())
+						.attr("text-anchor", "middle")
+						.attr("dominant-baseline", "central")
+						.attr("pointer-events", "none")
+						.attr("font-size", "15px")
+						.attr("fill", "#5f9ea0");
+
+					return group;
+				},
+				(update) => update,
+				(exit) => exit.remove()
+			)
+			.attr("transform", (d) => `translate(${d.cx}, ${d.cy})`)
+			.call(
+				d3
+					.drag<SVGGElement, Vertex>()
+					.on("start", dragStarted)
+					.on("drag", dragged)
+					.on("end", dragEnded)
+			)
+			.on("click", function (event: MouseEvent, d) {
+				event.stopPropagation();
+
+				if (isAddEdgeMode && d.id !== highlightedElement?.id) {
+					if (!highlightedElement) return;
+					const sourceNodeId = highlightedElement.id;
+					if (typeof sourceNodeId !== "number") return;
+
+					if (
+						!edgesSet.has(`${d.id}-${sourceNodeId}`) &&
+						!edgesSet.has(`${sourceNodeId}-${d.id}`)
+					) {
+						const newEdge = `${d.id}-${sourceNodeId}`;
+						edgesSet.add(newEdge);
+						setEdges((prevEdges) => [
+							...prevEdges,
+							[d.id, sourceNodeId],
+						]);
+					}
+
+					setIsAddEdgeMode(false);
+					setHighlightedElement(null);
+					return;
+				}
+
+				if (
+					highlightedElement?.type === "node" &&
+					highlightedElement.id === d.id
+				) {
+					setHighlightedElement(null);
+					setIsAddEdgeMode(false);
+				} else {
+					setHighlightedElement({
+						type: "node",
+						id: d.id,
+					});
+				}
+			})
+			.classed(
+				"highlighted-node",
+				(d) =>
+					highlightedElement?.type === "node" &&
+					highlightedElement.id === d.id
+			)
+			.classed(
+				"glowing-node",
+				(d) => isAddEdgeMode === true && highlightedElement?.id !== d.id
+			);
+	}, [
+		vertices,
+		edges,
+		dragStarted,
+		dragged,
+		dragEnded,
+		highlightedElement,
+		isAddEdgeMode,
+		edgesSet,
+	]);
+
+	return (
+		<main
+			onClick={(event: React.MouseEvent) => {
+				setHighlightedElement(null);
+				setIsAddEdgeMode(false);
+			}}
+			className="flex items-center justify-center gap-1 w-screen xl:px-16 lg:px-10 px-4 min-h-screen"
+		>
+			<div
+				ref={svgContainerRef}
+				className="relative border-2 border-stone-300 flex-1 h-[700px] rounded-lg"
+			>
+				<div className="absolute border-2 border-stone-300 top-1 left-1/2 -translate-x-1/2 h-[50px] rounded-lg flex items-center gap-2 px-4">
+					<Button variant="ghost" size="icon">
+						<MousePointer2 className="text-stone-400" />
+					</Button>
+					<Separator
+						orientation="vertical"
+						className="h-[60%] bg-stone-300"
+					/>
+					<Button variant="ghost" size="icon">
+						<Grab className="text-stone-400" />
+					</Button>
+					<Separator
+						orientation="vertical"
+						className="h-[60%] bg-stone-300"
+					/>
+					<Button
+						onClick={handleAddVertex}
+						variant="ghost"
+						size="icon"
+					>
+						<Plus className="text-stone-400" />
+					</Button>
+					<Separator
+						orientation="vertical"
+						className="h-[60%] bg-stone-300"
+					/>
+					<Button
+						onClick={handleResetGraph}
+						variant="ghost"
+						size="icon"
+					>
+						<RotateCcw className="text-stone-400" />
+					</Button>
+				</div>
+
+				{isNodeSelected && (
+					<div className="absolute border-2 border-stone-300 bottom-1 left-1/2 -translate-x-1/2 h-[50px] rounded-lg flex items-center justify-between gap-2 px-4">
+						<Button variant="ghost" className="text-stone-400">
+							<Palette className="text-stone-400" /> Color
+						</Button>
+						<Separator
+							orientation="vertical"
+							className="h-[60%] bg-stone-300"
+						/>
+						<Button
+							onClick={handleAddEdge}
+							variant="ghost"
+							className="text-stone-400"
+						>
+							<Spline className="text-stone-400" /> Add Edge
+						</Button>
+						<Separator
+							orientation="vertical"
+							className="h-[60%] bg-stone-300"
+						/>
+						<Button variant="ghost" className="text-stone-400">
+							<Trash2 className="text-stone-400" /> Delete
+						</Button>
+					</div>
+				)}
+			</div>
+
+			<div className="grid grid-rows-3 gap-1 w-[500px] h-[700px] rounded-lg">
+				<div className="border-2 border-stone-300 w-full row-span-2 rounded-lg overflow-scroll">
+					<CodeSnippet />
+				</div>
+				<div className="border-2 border-stone-300 w-full row-span-1 rounded-lg"></div>
+			</div>
+		</main>
+	)
+}
